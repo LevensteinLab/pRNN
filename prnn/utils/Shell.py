354b57bf3081b0b5cc0db92956fe71d0bf9ee221
AlexEfremov <aleksei.efremov@mila.quebec>
2025-05-14 11:23:03 -0400

    Temporary datafolder + bugfix

--- prnn/utils/Shell.py
+++ prnn/utils/Shell.py
@@ -1032,7 +1032,7 @@ class RiaBColorsGridShell(RiaBVisionShell):
             vel = self.ag.vel
             pos = self.ag.pos
 
-        if vel:
+        if vel is not None:
             self.ag.pos = pos
         else:
             vel = [0,0]

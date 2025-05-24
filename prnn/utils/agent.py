399361a32580afe6a4f0ed9acc4d7d7cc27d9995
AlexEfremov <aleksei.efremov@mila.quebec>
2025-05-24 14:23:43 -0400

    Made AE variational

--- prnn/utils/agent.py
+++ prnn/utils/agent.py
@@ -9,6 +9,8 @@ Created on Fri Nov 12 20:05:03 2021
 import numpy as np
 
 from numpy.random import choice
+from ratinabox.utils import get_angle
+from ratinabox.Agent import Agent
 
 def randActionSequence(tsteps,action_space,action_probability):
     
@@ -139,6 +141,98 @@ class RatInABoxAgent:
                                                         discretize, inv_x, inv_y)
 
         return obs, act, state, render
+
+
+class MiniworldRandomAgent(Agent):        
+    def __init__(self, riab_env, params={
+                                    "dt": 0.1,
+                                    "speed_mean": 0.2,
+                                    "thigmotaxis": 0.2,
+                                    "wall_repel_distance": 0.2,
+                                    }):
+        
+        super().__init__(riab_env, params)
+        self.reset()
+
+    def update(self, dt=None, drift_velocity=None, drift_to_random_strength_ratio=1):
+        super().update(dt, drift_velocity, drift_to_random_strength_ratio)
+        self.history["speed"].append(
+            np.linalg.norm(np.array(self.history["pos"][-1]) - np.array(self.history["pos"][-2]))
+        )
+
+        angle_now = get_angle(np.array(self.history["pos"][-1]) - np.array(self.history["pos"][-2]))
+        angle_before = self.history["angle"][-1]
+        if abs(angle_now - angle_before) > np.pi:
+            if angle_now > angle_before:
+                angle_now -= 2 * np.pi
+            elif angle_now < angle_before:
+                angle_before -= 2 * np.pi
+        self.history["rotation"].append(angle_now - angle_before)
+        self.history["angle"].append(angle_now)
+        return
+
+
+    def generateActionSequence(self, pos, direction, T=1000):
+        self.pos = pos
+        self.velocity = self.speed_std * np.array([np.cos(direction), np.sin(direction)])
+        self.history["pos"] = [self.pos]
+        self.history["vel"] = [self.velocity]
+        self.history["speed"] = [np.linalg.norm(self.velocity)]
+        self.history["angle"] = [get_angle(self.velocity)]
+
+        for i in range(T):
+            self.update()
+
+        traj = np.vstack((np.array(self.history["speed"]) * 10, np.array(self.history["rotation"])))
+
+        return traj[:, -T:]
+    
+    def getObservations(self, env, tsteps, reset=True, includeRender=False, **kwargs):   
+        """
+        Get a sequence of observations. act[t] is the action after observing
+        obs[t], obs[t+1] is the resulting observation. obs will be 1 entry 
+        longer than act
+        """
+        act = self.generateActionSequence(tsteps)
+
+        render = False
+            
+        obs = [None for t in range(tsteps+1)]
+        if reset:
+            obs[0] = env.reset()
+        else:
+            o = env.env.gen_obs()
+            obs[0] = env.env.observation(o)
+        state = {'agent_pos': np.resize(env.get_agent_pos(),(1,2)), 
+                 'agent_dir': env.get_agent_dir()
+                }
+        if includeRender:
+            render = [None for t in range(tsteps+1)]
+            render[0] = env.render(mode=None)
+            
+        for aa in range(tsteps):
+            obs[aa+1] = env.step(act[aa])[0]
+            state['agent_pos'] = np.append(state['agent_pos'],
+                                           np.resize(env.get_agent_pos(),(1,2)),axis=0)
+            state['agent_dir'] = np.append(state['agent_dir'],
+                                           env.get_agent_dir())
+            if includeRender:
+                render[aa+1] = env.render(mode=None)
+                
+        act = np.ones_like(act) * self.constantAction
+
+        return obs, act, state, render
+    
+    def reset(self):
+        self.reset_history()
+        self.initialise_position_and_velocity()
+        self.history["t"] = [0]
+        self.history["pos"] = [self.pos]
+        self.history["vel"] = [self.velocity]
+        self.history["rot_vel"] = [self.rotational_velocity]
+        self.history["speed"] = [np.linalg.norm(self.velocity)]
+        self.history["rotation"] = [0]
+        self.history["angle"] = [get_angle(self.velocity)]
     
 
 def create_agent(envname, env, agentname):

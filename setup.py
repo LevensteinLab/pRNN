eb4b2497e56f16793381f4c267e1a0d404e030a6
AHalawa99 <adel.halawa@mail.mcgill.ca>
2025-09-11 08:53:20 -0400

    add emtpy init.py files for smoother importing

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
 from setuptools import setup, find_packages
 
+# dependencies = [
+#     "pandas==1.5.3",
+#     "matplotlib==3.5.3",
+#     "scipy==1.12.0",
+#     "numpy==1.22.4",
+#     "scikit-learn==1.4.0",
+#     "pynapple==0.6.1",
+#     "zipp==3.17.0",
+#     "pyparsing==3.1.1",
+#     "importlib_metadata==7.0.1",
+#     "ruamel.yaml==0.18.6",
+#     #"gym==0.21.0 --no-binary :all:", TODO: Need to fix this
+#     "gymnasium==0.29.1",
+#     "ratinabox==1.7.1"
+# ]
 
 dependencies = [
-    "pandas==1.5.3",
-    "matplotlib==3.5.3",
-    "scipy==1.12.0",
-    "numpy==1.22.4",
-    "scikit-learn==1.4.0",
+    "pandas",
+    "matplotlib",
+    "scipy",
+    "numpy",
+    "scikit-learn",
     "pynapple==0.6.1",
-    "zipp==3.17.0",
-    "pyparsing==3.1.1",
-    "importlib_metadata==7.0.1",
-    "ruamel.yaml==0.18.6",
+    "zipp",
+    "pyparsing",
+    "importlib_metadata",
+    "ruamel.yaml",
     #"gym==0.21.0 --no-binary :all:", TODO: Need to fix this
     "gymnasium==0.29.1",
     "ratinabox==1.7.1"
